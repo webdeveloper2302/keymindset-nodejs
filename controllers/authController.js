3c88b15c127173df2cd544ff4bee1c822263a00b
Priya <priyaindiit@gmail.com>
2026-09-23 14:39:37 +0530

    lats

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const UserDetails = require("../models/UserDetails");
 const AdminRequest = require("../models/AdminRequest");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
